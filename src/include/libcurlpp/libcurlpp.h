f3d1a1d3262215f41a5198f82a483494f0b301ba
FelipeNishino <felipenishino@gmail.com>
2022-09-28 17:45:17 -0300

    Libs movidas para pastas separadas

---
+++ src/include/libcurlpp/libcurlpp.h
@@ -0,0 +1,16 @@
+#ifndef LIBCURL_GENERALHEADER_H
+#define LIBCURL_GENERALHEADER_H
+
+#include "curlpp/cURLpp.hpp"
+#include "curlpp/Types.hpp"
+#include "curlpp/Options.hpp"
+#include "curlpp/OptionBase.hpp"
+#include "curlpp/Easy.hpp"
+#include "curlpp/Exception.hpp"
+#include "curlpp/Form.hpp"
+#include "curlpp/Info.hpp"
+#include "curlpp/Multi.hpp"
+#include "curlpp/Options.hpp"
+#include "curlpp/Infos.hpp"
+
+#endif //LIBCURL_GENERALHEADER_H
---
+++ src/include/libmidifile/libmidifile.h
@@ -0,0 +1,11 @@
+#ifndef MIDIFILE_GENERALHEADER_H
+#define MIDIFILE_GENERALHEADER_H
+
+#include "Binasc.h"
+#include "MidiEvent.h"
+#include "MidiEventList.h"
+#include "MidiFile.h"
+#include "MidiMessage.h"
+#include "Options.h"
+
+#endif //MIDIFILE_GENERALHEADER_H
