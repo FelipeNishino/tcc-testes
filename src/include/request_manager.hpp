d0e5176417b2f177aba17c000ba1956eba7a8a33
FelipeNishino <felipenishino@gmail.com>
2022-10-02 00:37:56 -0300

    Implementacão de requests da API do Spotify

--- src/include/request_manager.hpp
+++ src/include/request_manager.hpp
@@ -2,18 +2,27 @@
 #define TCC_REQUESTMANAGER_H
 
 #include <string>
-#include "libcurlpp/libcurlpp.h"
 #include <chrono>
+#include <vector>
+#include "libcurlpp/libcurlpp.h"
+
+static const std::string SPOTIFY_ACCOUNT_URL = "https://accounts.spotify.com/api";
+static const std::string SPOTIFY_BASE_URL = "https://api.spotify.com/v1";
 
 class RequestManager {
     private:
         std::string authorization;
         std::string auth_token;
-        std::chrono::seconds auth_expiration;
+        std::chrono::time_point<std::chrono::system_clock> auth_expiration;
         void retrieve_auth_token();
+        void read_song_list();
     public:
         RequestManager();
-        void perform_request();
+        ~RequestManager();
+        void perform_request(curlpp::Easy *req);
+        void request_track_feature_by_id(std::string song_id);
+        void request_track_feature_by_ids(std::vector<std::string> song_id={});
+
 };
 
 #endif //TCC_REQUESTMANAGER_H
--- src/request_manager.cpp
+++ src/request_manager.cpp
@@ -3,9 +3,16 @@
 #include <chrono>
 #include <exception>
 #include <cstdlib>
+#include <ostream>
 #include <stdexcept>
 #include <vector>
+#include <sstream>
+#include <cerrno>
+#include <fstream>
+#include "include/libjson/json.hpp"
 
+using std::chrono::seconds;
+using std::chrono::duration_cast;
 
 struct no_environment_vars : public std::exception {
    	const char * what () const throw () {
@@ -14,12 +21,59 @@ struct no_environment_vars : public std::exception {
 };
 
 void RequestManager::retrieve_auth_token() {
-	
+	try {
+    	curlpp::Easy request;
+		using namespace curlpp::Options;
+		nlohmann::json result_json;
+		std::list<std::string> header {
+			"Content-Type: application/x-www-form-urlencoded",
+			"Authorization: " + authorization
+		}; 
+		std::string param = "grant_type=client_credentials";
+		std::ostringstream result_stream;	
+		std::vector<curlpp::OptionBase *> opt{
+			new Url(SPOTIFY_ACCOUNT_URL + "/token"),
+			new Verbose(false),
+			new HttpHeader(header),
+			new PostFields(param),
+			new PostFieldSize(param.size()),
+			new WriteStream(&result_stream)
+		};
+		
+		request.setOpt(opt.begin(), opt.end());
+
+    	request.perform();
+		long response_code = curlpp::infos::ResponseCode::get(request);
+		switch (response_code) {
+			case 200 ... 299:
+				std::cout << "Sucesso: " << response_code << '\n';
+				result_json = nlohmann::json::parse(result_stream.str());
+				auth_token = "Bearer " + std::string(result_json["access_token"]);
+				auth_expiration = std::chrono::system_clock::now();
+				break;
+			case 400 ... 499:
+				std::cout << "Request error: " << response_code << '\n';
+				break;
+			case 500 ... 599:
+				std::cout << "Server error" << response_code << '\n';
+				break;		
+			default:
+				break;
+		}
+  	}
+	catch ( curlpp::LogicError & e ) {
+		std::cout << e.what() << std::endl;
+	}
+	catch ( curlpp::RuntimeError & e ) {
+		std::cout << e.what() << std::endl;
+	}
 }
 
 RequestManager::RequestManager() {
 	std::string client_id;
 	std::string client_secret;
+
+	curlpp::initialize();
 
 	try {
 		client_id = std::string(std::getenv("SPOTIFY_CLIENT_ID"));
@@ -40,11 +94,115 @@ RequestManager::RequestManager() {
 	retrieve_auth_token();
 }
 
-void RequestManager::perform_request() {
-    const auto p1 = std::chrono::system_clock::now();
-
-    std::cout << "seconds since epoch: "
-              << std::chrono::duration_cast<std::chrono::seconds>(
-                   p1.time_since_epoch()).count() << '\n';
+RequestManager::~RequestManager() {
+	cURLpp::terminate();
 }
 
+void RequestManager::read_song_list() {
+
+}
+
+void RequestManager::request_track_feature_by_ids(std::vector<std::string> song_id) {
+		using namespace curlpp::Options;
+    	
+		if (song_id.empty()) {
+			read_song_list();
+		}
+
+		curlpp::Easy request;
+		nlohmann::json result_json;
+		std::list<std::string> header {
+			"Content-Type: application/json",
+			"Authorization: " + auth_token,
+		}; 
+		std::ostringstream result_stream;
+		std::string ids;
+		for (std::string &s : song_id) {
+			ids.append(s + ",");	
+		}
+		std::vector<curlpp::OptionBase *> opt{
+			new Url(SPOTIFY_BASE_URL + "/audio-features?ids=" + ids),
+			new HttpHeader(header),
+			new HttpGet(true),
+			new Verbose(false)
+			// new WriteStream(&result_stream)
+		};
+		
+		request.setOpt(opt.begin(), opt.end());
+
+		perform_request(&request);
+		
+		long response_code = curlpp::infos::ResponseCode::get(request);
+		switch (response_code) {
+			case 200 ... 299:
+				// std::cout << "Sucesso: " << response_code << '\n';
+				// result_json = nlohmann::json::parse(result_stream.str());
+				break;
+			case 400 ... 499:
+				std::cout << "Request error: " << response_code << '\n';
+				// std::cout << curlpp::infos::EffectiveUrl::get(request);
+				break;
+			case 500 ... 599:
+				std::cout << "Server error" << response_code << '\n';
+				break;		
+			default:
+				break;
+		}
+}
+
+void RequestManager::request_track_feature_by_id(std::string song_id) {
+	curlpp::Easy request;
+	using namespace curlpp::Options;
+	nlohmann::json result_json;
+	std::list<std::string> header {
+		"Content-Type: application/json",
+		"Authorization: " + auth_token,
+	}; 
+	std::ostringstream result_stream;
+
+	std::vector<curlpp::OptionBase *> opt{
+		new Url(SPOTIFY_BASE_URL + "/audio-features/" + song_id),
+		new HttpHeader(header),
+		new HttpGet(true),
+		// new WriteStream(&result_stream)
+	};
+	
+	request.setOpt(opt.begin(), opt.end());
+
+	perform_request(&request);
+
+	long response_code = curlpp::infos::ResponseCode::get(request);
+	switch (response_code) {
+		case 200 ... 299:
+			// std::cout << "Sucesso: " << response_code << '\n';
+			// result_json = nlohmann::json::parse(result_stream.str());
+			break;
+		case 400 ... 499:
+			std::cout << "Request error: " << response_code << '\n';
+			break;
+		case 500 ... 599:
+			std::cout << "Server error" << response_code << '\n';
+			break;		
+		default:
+			break;
+	}
+  	
+}
+
+void RequestManager::perform_request(curlpp::Easy *req) {
+	const auto interval = duration_cast<seconds>(auth_expiration.time_since_epoch());
+	// TODO: N TA FUNFANDO
+	if (interval.count() > 3600) {
+		// retrieve_auth_token();
+	}
+
+	try {
+    	req->perform();
+  	}
+	catch ( curlpp::LogicError & e ) {
+		std::cout << e.what() << std::endl;
+	}
+	catch ( curlpp::RuntimeError & e ) {
+		std::cout << e.what() << std::endl;
+	}
+}
